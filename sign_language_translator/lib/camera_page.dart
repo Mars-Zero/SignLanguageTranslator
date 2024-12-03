a7a7661779f1aa464be176a1f876af6279f9c98c
banescuema101 <banescuema@gmail.com>
2024-12-03 18:17:49 +0200

    I have added the 'Help' button, centered on the same line as the 'Start Translation' button. When pressed, it opens an instruction box, which in turn has a title, text content, and a close button.

--- sign_language_translator/lib/camera_page.dart
+++ sign_language_translator/lib/camera_page.dart
@@ -6,6 +6,7 @@ class CameraPage extends StatelessWidget {
     super.key,
   });
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +31,21 @@ class CameraPage extends StatelessWidget {
               child: Camera(),
             ),
             const SizedBox(height: 110),
+            // plasez butonul de "Start Translation si cel de Help pe aceeasi linie."
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // aici sa am butonul de start translation, in partea stanga a ecranului.
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Start Translation'),
+              ),
+            // un padding de 20 px stanga dreapta intre cele doua butoane.
+            const SizedBox(width: 20), 
             ElevatedButton(
-              onPressed: () {},
-              child: const Text('Start Translation'),
+              onPressed: () {showInstructionsDialog(context);},
+              child: const Text('Help'),
+            ),
+          ],
             ),
             Text(
               'Sign Language Translator',
@@ -45,6 +58,33 @@ class CameraPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  // Metoda pentru afisarea unei casute de instructiuni, widget care are atat titlu,
+  // parte de continut(text), dar si buton de inchidere, "Close"
+  void showInstructionsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Instrucțiuni de folosire'),
+          content: const Text(
+            'Urmati pasii urmatori:\n'
+            '1. Utilizati camera pentru a captura semnele.\n'
+            '2. Exectutati semnele cat mai clar si lasati sa treaca macar o secunda intre doua reprezentari consecutive.\n'
+            '3. Daca doriti rotirea camerei, apasati pe butonul din coltul din dreapta, sus.\n'
+            '4. Apasati butonul Start Translation pentru a incepe procesul de translatarea\n'
+            ,
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () { Navigator.of(context).pop(); }, // inchid dialogul
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
