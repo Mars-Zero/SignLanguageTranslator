ca79cb15b6a575c646af8f167e42e980bb37adb7
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2024-12-07 20:51:28 +0200

    loading from opencv

--- AI/main.py
+++ AI/main.py
@@ -4,6 +4,9 @@ from openai import OpenAI
 import requests
 import transformers
 from huggingface_hub import login
+from PIL import Image
+import cv2
+
 
 
 def call_huggingface_model(prompt):
@@ -11,11 +14,26 @@ def call_huggingface_model(prompt):
     messages = [
         {"role": "user", "content": "Who are you?"},
     ]
-    pipe = transformers.pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
+    pipe = transformers.pipeline("text-generation", model="mistralai/Mistral-Nemo-Base-2407")   
     response = pipe(messages)
     return response
 
+def classify_image(image_opencv):
+    login(token="")
+    pipe = transformers.pipeline("image-classification", model="RavenOnur/Sign-Language")
+    #image = Image.open(image_path)
+    image = Image.fromarray(cv2.cvtColor(image_opencv, cv2.COLOR_BGR2RGB))
+    result = pipe(image)
+    return result
 
 if __name__ == '__main__':
-    print(call_huggingface_model("Hi, my name is"))
+    cap = cv2.VideoCapture(0)
+    w, h = 360, 240
+    while True:
+        _, img = cap.read()
+        img = cv2.resize(img, (w,h))
+        cv2.imshow("Camera",img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        print(classify_image(img))
 
