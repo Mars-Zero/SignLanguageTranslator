7153e7b716eead866ec756ca5cd02406bd1c95c6
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2024-12-07 22:50:27 +0200

    added a training module

--- AI/install.sh
+++ AI/install.sh
@@ -2,6 +2,7 @@ python3 -m venv .venv
 source ./.venv/bin/activate
 
 pip install openai
+pip install mediapipe-model-maker
 pip install tensorflow==2.2.0
 pip install transformers==3.3.1s
 #pip install transformers
