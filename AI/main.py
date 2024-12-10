8fb5c34ada6cbee2a67942047ef537cee2fc29fb
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2024-12-10 18:05:37 +0200

    added openai api call in case huggingface fails

--- AI/main.py
+++ AI/main.py
@@ -10,17 +10,34 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+client = OpenAI(
+    api_key=""
+)
+def call_openai_model(prompt):
+    messages = []
+    messages.append(
+        {"role": "system", "content": "You are an AI content writer helper. Your role is to act as an assistant and correct errors. Do not add further explanation, only return the corrected text."}
+    )
+    messages.append(
+        {"role": "user", "content": prompt}
+
+    )
+    response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+    )
+    return response.choices[0].message
 
 def call_huggingface_model(prompt):
     login(token="")
     messages = [
-        {"role": "user", "content": "You are an AI model that corrects errors in writing. Please correct the following input "+prompt},
+        {"role": "user", "content": "You are an AI model that corrects errors in writing. Do not add any other details, just correct the input. Please correct the following input "+prompt},
     ]
-    pipe = transformers.pipeline("text-generation", model="mistralai/Mistral-Nemo-Base-2407")   
+    pipe = transformers.pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")   
     response = pipe(messages)
     return response
 
-def classify_image(image_opencv):
+def classify_image_huggingface(image_opencv):
     login(token="")
     pipe = transformers.pipeline("image-classification", model="RavenOnur/Sign-Language")
     image = Image.fromarray(cv2.cvtColor(image_opencv, cv2.COLOR_BGR2RGB))
@@ -28,29 +45,24 @@ def classify_image(image_opencv):
     return result
 
 
-base_options = python.BaseOptions(model_asset_path='./AI/training/training/gesture_recognizer_trained_large_dataset.task')
+base_options = python.BaseOptions(model_asset_path='./AI/training/gesture_recognizer_trained_large_dataset.task')
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
 def classify_image(image_opencv):
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_opencv)
     recognition_result = recognizer.recognize(mp_image)
-
-    print(recognition_result.gestures)
-    print(recognition_result.hand_landmarks)
-
-    top_gesture = recognition_result.gestures[0][0]
-    return top_gesture
-    #hand_landmarks = recognition_result.hand_landmarks
+    return recognition_result.gestures
     
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)
-    w, h = 360, 240
-    while True:
-        _, img = cap.read()
-        img = cv2.resize(img, (w,h))
-        cv2.imshow("Camera",img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        print(classify_image(img))
+    print(call_openai_model("HHhhhhheeeelllllloooooo"))
+    # cap = cv2.VideoCapture(0)
+    # w, h = 360, 240
+    # while True:
+    #     _, img = cap.read()
+    #     img = cv2.resize(img, (w,h))
+    #     cv2.imshow("Camera",img)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #     print(classify_image(img))
 
