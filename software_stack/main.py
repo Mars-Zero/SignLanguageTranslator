45e8304acaf2296315917774c4cce0c44c917bd4
banescuema101 <banescuema@gmail.com>
2024-12-11 01:49:19 +0200

    Create flask server that uploads the pictures

--- software_stack/main.py
+++ software_stack/main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from flask import Flask, request, jsonify
+import os
+import uuid
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+# setez directorul in care voi incarca pozele primite de camera din aplicatia flutter.
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ['jpg', 'png', 'jpeg']
 
+# Funcție pentru a verifica dacă fișierul are extensia corectă
+def check_filename(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def home():
+    return "Serverul Flask merge, merge!"
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # verific daca in cerere exista un fisier.
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
-# Press the green button in the gutter to run the script.
+    # preiau FISIERUL din request.
+    file = request.files['file']
+
+    if file.filename == '': 
+        # in caz ca am un fisier gol:
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file and check_filename(file.filename):
+        cod_unic = uuid.uuid4().hex
+        # am creat un hash unic pe care sa il apenduiesc in fata numelui fisierului, pentru
+        # ca daca il lasam fara, fisierele nu mai aveau denumiri distincte, si folderul
+        # ramanea mereu doar cu o poza, la fiecare incarcare a camerei din flutter.
+        filename = f"{cod_unic}{file.filename}"
+        # concatenez cu un contor pentru ca altfel, imi punea pozele
+        # distincte ca acelasi obiect file in folder.
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print(f"File saved: {os.path.join(app.config['UPLOAD_FOLDER'], filename)}")  # Debugging
+        return jsonify({'message': 'File uploaded successfully!'}), 200
+    else:
+        return jsonify({'error': 'File type not allowed'}), 400
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(debug=True, host='0.0.0.0', port=5000)
